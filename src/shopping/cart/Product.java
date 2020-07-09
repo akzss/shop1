03ea95b8ed2080c5964f6d277c8c969fe47ae0c5
akzss <37257040+akzss@users.noreply.github.com>
2020-07-09 16:53:02 +0800

    Add files via upload

---
+++ src/shopping/cart/Product.java
@@ -0,0 +1,43 @@
+package shopping.cart;
+import java.io.Serializable;
+
+public class Product implements Serializable {
+    private String id;
+    private String name;
+    private double price;
+
+    public Product() {
+    }
+
+    public Product(String id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
